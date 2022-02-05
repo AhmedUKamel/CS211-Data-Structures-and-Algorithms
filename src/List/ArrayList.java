bdf8b37a28f3843fefe3226c228e619a13a860f3
Ahmed Kamel <85279324+AhmedKamel188@users.noreply.github.com>
2022-02-05 10:28:06 +0200

    Updated

--- src/List/ArrayList.java
+++ src/List/ArrayList.java
@@ -11,6 +11,25 @@ public class ArrayList <type> {
     }
 
     // methods
+    // methods
+    /*---------------------------------------------- Insertion ----------------------------------------------*/
+    /*
+     * 1: insertFirst() takes only element to add it in begin of list
+     * 2: insert() takes only element to add it in end of list
+     * 3: insert() takes value and specific index to add in
+     * 4: insertNoDublicate() method takes a value as parameter and insert it only it's not exist in arraylist
+     * */
+    public void insertFirst(type element) {
+        if (isFull())
+            System.out.println("List is Overflow");
+        else {
+            for (int i = end + 1; i > 0;)
+                list[i] = list[--i];
+            list[0] = element;
+            end++;
+        }
+    }
+
     public void insert(type element) {
         if (isFull())
             System.out.println("List is Overflow");
@@ -32,6 +51,21 @@ public class ArrayList <type> {
         }
     }
 
+    public void insertNoDublicate(type element) {
+        if (search(element) == -1)
+            insert(element);
+    }
+
+    /*---------------------------------------------- Removal ----------------------------------------------*/
+
+    /*
+     * 1: remove() takes index as a parameter to remove it
+     * 2: overloaded remove() doesn't take parameters and remove the first element in list
+     * 3: pop() doesn't take parameters and remove the last element in list
+     * 4: clear() delete all elements from list
+     * */
+
+
     public void remove(int position) {
         if (!isValidPosition(position))
             System.out.println("Invalid range");
@@ -44,6 +78,26 @@ public class ArrayList <type> {
         }
     }
 
+    public void remove() {
+        if (isEmpty())
+            System.out.println("List is Underflow");
+        else {
+            for (int i = 0; i < end;)
+                list[i] = list[i++];
+            end--;
+        }
+    }
+
+    public void pop() {
+        end--;
+    }
+
+    public void clear() {
+        end = -1;
+    }
+
+    /*---------------------------------------------- Update ----------------------------------------------*/
+
     public void update(int position, type element) {
         if (!isValidPosition(position))
             System.out.println("Invalid range");
@@ -53,24 +107,21 @@ public class ArrayList <type> {
             list[position] = element;
     }
 
-    public type get(int position) {
-        if (!isValidPosition(position))
-            System.out.println("Invalid range");
-        else if (isEmpty())
-            System.out.println("List is Underflow");
-        else
-            return list[position];
-        return null;
+    /*---------------------------------------------- Display ----------------------------------------------*/
+
+    /*
+     * display() method takes a boolean input to print the elements ( nodes ) in two different orders ( ascending / descending ).
+     * in case of list contains no elements
+     * */
+
+    public void display() {
+        System.out.print("[ ");
+        for (int i = 0; i <= end;)
+            System.out.print(list[i++] + " ");
+        System.out.println("]");
     }
 
-    public void insertNoDublicate(type element) {
-        if (search(element) == -1)
-            insert(element);
-    }
-
-    public void clear() {
-        end = -1;
-    }
+    /*---------------------------------------------- Search ----------------------------------------------*/
 
     public int search(type element) {
         if (isEmpty())
@@ -82,15 +133,38 @@ public class ArrayList <type> {
         return -1;
     }
 
-    public void display() {
-        System.out.print("[ ");
-        for (int i = 0; i <= end;)
-            System.out.print(list[i++] + " ");
-        System.out.println("]");
+    /*---------------------------------------------- Access ----------------------------------------------*/
+
+    public type get(int position) {
+        if (!isValidPosition(position))
+            System.out.println("Invalid range");
+        else if (isEmpty())
+            System.out.println("List is Underflow");
+        else
+            return list[position];
+        return null;
     }
+
+    /*---------------------------------------------- Other ----------------------------------------------*/
+
+    /*
+     * 1: isValidPosition() method help you to determine if the given position is in range or not (* range validation *).
+     * 2: size() method just return the maximum size allowed for arraylist.
+     * 3: capacity() method just return the actual number of elements ( nodes ) in your arraylist.
+     * 4: isEmpty() method return if the list contain at least element or not (* empty or not *).
+     * 5: isFull() method return if the list contain the maximum allowed number or not (* empty or not *).
+     * */
 
     public boolean isValidPosition(int pos) {
         return pos >= 0 && pos < size();
+    }
+
+    public int size() {
+        return list.length;
+    }
+
+    public int capacity() {
+        return end + 1;
     }
 
     public boolean isEmpty() {
@@ -99,13 +173,5 @@ public class ArrayList <type> {
 
     public boolean isFull() {
         return end == size() - 1;
-    }
-
-    public int capacity() {
-        return end + 1;
-    }
-
-    public int size() {
-        return list.length;
     }
 }
--- src/List/DoublyLinkedList.java
+++ src/List/DoublyLinkedList.java
@@ -1,9 +1,10 @@
 package List;
-public class SinglyLinkedList <type> {
+public class DoublyLinkedList <type> {
     // attributes
     private class Node {
         private type value;
         private Node next;
+        private Node prev;
     }
     private Node head;
     private Node tail;
@@ -12,20 +13,22 @@ public class SinglyLinkedList <type> {
     // methods
     /*---------------------------------------------- Insertion ----------------------------------------------*/
     /*
-     * 1: insertFirst() takes only element to add it in begin of list
-     * 2: insert() takes only element to add it in end of list
-     * 3: insert() takes value and specific index to add in
-     * */
+    * 1: insertFirst() takes only element to add it in begin of list
+    * 2: insert() takes only element to add it in end of list
+    * 3: insert() takes value and specific index to add in
+    * */
 
     public void insertFirst(type element) {
         if (isEmpty()) {
             head = tail = new Node();
             head.value = element;
-        } else {
-            Node ptr = head;
-            head = new Node();
-            head.value = element;
-            head.next = ptr;
+        }
+        else {
+            Node newNode = new Node();
+            newNode.value = element;
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
         } ctr++;
     }
 
@@ -33,9 +36,12 @@ public class SinglyLinkedList <type> {
         if (isEmpty()) {
             head = tail = new Node();
             head.value = element;
-        } else {
-            tail = tail.next = new Node();
-            tail.value = element;
+        }
+        else {
+            tail.next = new Node();
+            tail.next.prev = tail;
+            tail.next.value = element;
+            tail = tail.next;
         } ctr++;
     }
 
@@ -45,8 +51,10 @@ public class SinglyLinkedList <type> {
             for (int i = 0; i < position - 1; i++)
                 ptr = ptr.next;
             Node newNode = new Node();
-            newNode.value = element;
             newNode.next = ptr.next;
+            newNode.prev = ptr;
+            newNode.value = element;
+            ptr.next.prev = newNode;
             ptr.next = newNode;
             ctr++;
         } else
@@ -56,43 +64,43 @@ public class SinglyLinkedList <type> {
     /*---------------------------------------------- Removal ----------------------------------------------*/
 
     /*
-     * 1: remove() takes index as a parameter to remove it
-     * 2: overloaded remove() doesn't take parameters and remove the first element in list
-     * 3: pop() doesn't take parameters and remove the last element in list
-     * 4: clear() delete all elements from list
-     * */
+    * 1: remove() takes index as a parameter to remove it
+    * 2: overloaded remove() doesn't take parameters and remove the first element in list
+    * 3: pop() doesn't take parameters and remove the last element in list
+    * 4: clear() delete all elements from list
+    * */
 
     public void remove(int position) {
-        if(isEmpty())
-            System.out.println("LinkedList is Underflow");
+        if (isEmpty())
+            System.out.println("List in Underflow");
         else if (!isValidPosition(position))
             System.out.println("Invalid range");
         else {
             Node ptr = head;
-            for (int i = 0; i < position - 1; i++)
+            for (int i = 0; i < position; i++)
                 ptr = ptr.next;
-            ptr.next = ptr.next.next;
+            ptr.prev.next = ptr.next;
+            ptr.next.prev = ptr.prev;
             ctr--;
         }
     }
 
     public void remove() {
-        if(isEmpty())
-            System.out.println("LinkedList is Underflow");
+        if (isEmpty())
+            System.out.println("List in Underflow");
         else {
             head = head.next;
+            head.prev = null;
             ctr--;
         }
     }
 
     public void pop() {
-        if(isEmpty())
-            System.out.println("LinkedList is Underflow");
+        if (isEmpty())
+            System.out.println("List in Underflow");
         else {
-            Node ptr = head;
-            while (ptr.next.next != null)
-                ptr = ptr.next;
-            ptr.next = null;
+            tail = tail.prev;
+            tail.next = null;
             ctr--;
         }
     }
@@ -120,39 +128,45 @@ public class SinglyLinkedList <type> {
     /*---------------------------------------------- Display ----------------------------------------------*/
 
     /*
-     * display() method takes a boolean input to print the elements ( nodes ) in two different orders ( ascending / descending ).
-     * in case of list contains no elements
-     * */
+    * display() method takes a boolean input to print the elements ( nodes ) in two different orders ( ascending / descending ).
+    * in case of list contains no elements
+    * */
 
-    public void display() {
+    public void display(boolean order) {
         System.out.print("[ ");
-        Node ptr = head;
-        while (ptr != null) {
-            System.out.print(ptr.value + " ");
-            ptr = ptr.next;
-        } System.out.println("]");
+        if (order) {
+            Node ptr = head;
+            while (ptr != null) {
+                System.out.print(ptr.value + " ");
+                ptr = ptr.next;
+            }
+        } else {
+            Node ptr = tail;
+            while (ptr != null) {
+                System.out.print(ptr.value + " ");
+                ptr = ptr.prev;
+            }
+        }
+        System.out.println("]");
     }
 
     /*---------------------------------------------- Search ----------------------------------------------*/
 
     public int search(type element) {
         Node ptr = head;
-        int idx = 0;
-        while (ptr != null) {
+        for (int i = 0; ptr != null; i++) {
             if (ptr.value.equals(element))
-                return idx;
-            else {
+                return i;
+            else
                 ptr = ptr.next;
-                idx++;
-            }
         } return -1;
     }
 
     /*---------------------------------------------- Access ----------------------------------------------*/
 
     public type get(int position) {
-        if(isEmpty())
-            System.out.println("LinkedList is Underflow");
+        if (isEmpty())
+            System.out.println("List in Underflow");
         else if (!isValidPosition(position))
             System.out.println("Invalid range");
         else {
@@ -160,20 +174,21 @@ public class SinglyLinkedList <type> {
             for (int i = 0; i < position; i++)
                 ptr = ptr.next;
             return ptr.value;
-        } return null;
+        }
+        return null;
     }
 
     /*---------------------------------------------- Other ----------------------------------------------*/
 
     /*
-     * 1: isValidPosition() method help you to determine if the given position is in range or not (* range validation *).
-     * 2: capacity() method just return the number of elements ( nodes ) in your list.
-     * 3: isEmpty() method return if the list contain at least element or not (* empty or not *).
-     * 4: reverse() method doesn't take parameters and only reverse elements order in list.
-     * */
+    * 1: isValidPosition() method help you to determine if the given position is in range or not (* range validation *).
+    * 2: capacity() method just return the number of elements ( nodes ) in your list.
+    * 3: isEmpty() method return if the list contain at least element or not (* empty or not *).
+    * 4: reverse() method doesn't take parameters and only reverse elements order in list.
+    * */
 
     private boolean isValidPosition(int pos) {
-        return pos >= 0 && pos <= ctr - 1;
+        return pos >= 0 && pos <+ ctr;
     }
 
     public int capacity() {
@@ -181,21 +196,18 @@ public class SinglyLinkedList <type> {
     }
 
     public boolean isEmpty() {
-        return head == null;
+        return ctr == 0;
     }
 
     public void reverse() {
-        if (!isEmpty()) {
-            Node cnt = tail = head;
-            Node prv = null;
-            Node nxt = cnt.next;
-            while (nxt != null) {
-                nxt = cnt.next;
-                cnt.next = prv;
-                prv = cnt;
-                cnt = nxt;
-            } head = prv;
-        } else
-            System.out.println("LinkedList is Underflow");
+        Node ptr = head;
+        while (ptr != null) {
+            Node swp = ptr.next;
+            ptr.next = ptr.prev;
+            ptr.prev = swp;
+            ptr = ptr.prev;
+        } Node tmp = head;
+        head = tail;
+        tail = tmp;
     }
 }
